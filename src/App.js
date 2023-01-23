b22baa8ba8ac0d443464dc53034b037292c50219
Supul Kalhara <supulkalhara7@gmail.com>
2023-01-23 23:25:54 +0530

    linking and home page ongoing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import  Header from './Components/Header';
 import './App.css';
+import AppRouter from './Components/AppRouter';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AppRouter />
     </div>
   );
 }
