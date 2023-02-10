ffb00eeed58584b1ce43cc0817e6194ad8a9707c
Supul Kalhara <supulkalhara7@gmail.com>
2023-02-10 17:26:40 +0530

    Navbar changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,116 +1,52 @@
-import * as React from "react";
-import { Link as RouterLink } from 'react-router-dom';
+import React from "react";
+import { makeStyles } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import Link from '@mui/material/Link';
+import IconButton from "@mui/material/IconButton";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
-function Header() {
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  logo: {
+    float: "right"
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Link component={RouterLink} to="/">
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <a href="/">
             <img
               src="uni_gadgets.png"
               alt="logo"
               component="a"
-              style={{ width: "40px", height: "30px", textDecoration: "none" }}
+              style={{ width: "40px", height: "30px" }}
             />
-          </Link>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            uniGadgets
-          </Typography>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            uniGadgets
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Link component={RouterLink} to="/contacts">
-              <Button
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                CONTACT US
-              </Button>
-            </Link>
-            <Button
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              ABOUT
-            </Button>       
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton  sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-            >
-                <MenuItem key={1} >
-                  <Typography textAlign="center">Profile</Typography>
-                </MenuItem>
-                <MenuItem key={2} >
-                  <Typography textAlign="center">Log Out</Typography>
-                </MenuItem>
-                <MenuItem key={3} >
-                  <Typography textAlign="center">Profile</Typography>
-                </MenuItem>
-            </Menu>
-          </Box>
+          </a>
+          <Typography variant="h6">My App</Typography>
+          <div className={classes.logo}>
+            <IconButton href="#">
+              <FacebookIcon />
+            </IconButton>
+            <IconButton href="#">
+              <TwitterIcon />
+            </IconButton>
+            <IconButton href="#">
+              <InstagramIcon />
+            </IconButton>
+          </div>
         </Toolbar>
-      </Container>
-    </AppBar>
+      </AppBar>
+    </div>
   );
 };
+
 export default Header;
-
-
