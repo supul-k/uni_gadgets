59ff32bcb01e82f64da9a72a680d56f47f69e19f
Supul Kalhara <supulkalhara7@gmail.com>
2023-03-01 17:09:22 +0530

    trying to start again as a new project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import Header from "./Components/Header";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import HomePage from "./Components/Home/HomePage";
-import Contact from "./Components/Contact";
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contacts" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      hi
     </div>
   );
 }
